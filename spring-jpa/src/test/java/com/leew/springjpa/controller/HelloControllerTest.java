a5894815742a710b36ea658699aa2b712a73f5f5
def47619 <def47619@gmail.com>
2024-05-02 16:02:35 +0900

    <refact> : Hello World Mock 테스트 수정 및 Application Configuration 분리
    - https://www.notion.so/leew-work/33-MySQL-12d0872e59f44b079838639650616335
    - SpringJpaApplication 메인 클래스에서는 `@EnableJpaAuditing`으로
    	- 반드시 실행할 때 모두 jpa 컨텍스트를 로딩하도록 되어 있음
    - 그러나 HelloControllerTest에서는
    	- MockMvcTest로, Jpa 관련 context를 로드하지 않고 설정하여
    	- Main 함수 실행 도중에 에러 발생
    - 해결 방법 3가지 :
    	1. MockBean으로 Jpa 메타 Context 로딩
    		- `@MockBean(JpaMetaModelMappingContext.class)`
    	2. 별도의 Configuration 파일 설정
    		- EnableJpaAuditing 설정을 configuration 클래스에 annotation으로 추가 후
    		- `@Configuration`으로 등록한다.
    	3. 테스트 시 slice test만 한다.
    		- 테스트 코드에서 WebApplicationContext를 객체로 받고
    		- SpringBootTest 모두 로딩한 후
    		- 그 중에서 MockMvcBuilder로 MockMvc 객체 받아 테스트 수행

--- spring-jpa/src/test/java/com/leew/springjpa/controller/HelloControllerTest.java
+++ spring-jpa/src/test/java/com/leew/springjpa/controller/HelloControllerTest.java
@@ -1,20 +1,35 @@
 package com.leew.springjpa.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest
+// @MockBean(JpaMetamodelMappingContext.class)
+// @SpringBootTest
 class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+    // 위 wac를 사용하면 MockMvc를 Autowired 하지 않아도 실행 가능하다.
+    // @BeforeEach
+    void before() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
     void HelloWorld() throws Exception {
